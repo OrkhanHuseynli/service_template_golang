05b9f9b5d6d2a74f13e71d012149526d0ee98b16
Orkhan Huseynli <orkhan.huseynli@deutsche-boerse.com>
2020-02-10 08:54:40 +0000

    test init

--- src/handlers/serviceHandler_test.go
+++ src/handlers/serviceHandler_test.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewServiceHandler(t *testing.T) {
+	handler := NewServiceHandler(NewServiceSubHandler())
+	request := httptest.NewRequest("POST", "/product", nil)
+	response := httptest.NewRecorder()
+
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusBadRequest {
+		t.Errorf("Expected BadRequest got %v", response.Code)
+	}
+}
