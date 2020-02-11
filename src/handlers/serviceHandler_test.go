f5ca024487718926342e35b000dab11cb9776800
Orkhan Huseynli <orkhan.huseynli@deutsche-boerse.com>
2020-02-11 13:50:40 +0000

    test upgrade

--- src/handlers/serviceHandler_test.go
+++ src/handlers/serviceHandler_test.go
@@ -1,19 +1,31 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestNewServiceHandler(t *testing.T) {
-	handler := NewServiceHandler(NewServiceSubHandler())
-	request := httptest.NewRequest("POST", "/product", nil)
-	response := httptest.NewRecorder()
+func TestNewServiceHandlerWhenNoSearchCriteriaIsSet(t *testing.T) {
+	request, response, handler :=setupTest(nil)
 
 	handler.ServeHTTP(response, request)
 
 	if response.Code != http.StatusBadRequest {
 		t.Errorf("Expected BadRequest got %v", response.Code)
 	}
+}
+
+func setupTest(d interface{})(*http.Request, *httptest.ResponseRecorder, http.Handler){
+	handler := NewServiceHandler(NewServiceSubHandler())
+	request := httptest.NewRequest("POST", "/product", nil)
+	if d != nil {
+		body, _ := json.Marshal(d)
+		bodyReader :=  bytes.NewReader(body)
+		request = httptest.NewRequest("POST", "/product", bodyReader)
+	}
+	response := httptest.NewRecorder()
+	return request, response, handler
 }
